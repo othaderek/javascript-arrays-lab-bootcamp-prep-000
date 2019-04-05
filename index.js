2a8e32bff5e1af90ca62c6219a5873ca9bd69f80
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-05 20:42:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,11 +24,12 @@ function destructivelyRemoveFirstKitten() {
 
 function appendKitten(name) {
   kittens = [...kittens, name]
-  
+  return kittens
 }
 
 function prependKitten(name) {
-  
+  kittens = [name, ...kittens]
+  return kittens
 }
 
 function removeLastKitten() {
